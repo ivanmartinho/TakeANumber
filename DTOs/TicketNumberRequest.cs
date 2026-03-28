cf8ce2f2102dfd9fe7b91c2a50836b99396d8542
ivanmartinho <96298254+ivanmartinho@users.noreply.github.com>
2026-03-28 08:45:43 -0300

    Correção do request: Não tem que informar se o ticket foi chamado ou atendido. Quem fará este controle é a API

--- DTOs/TicketNumberRequest.cs
+++ DTOs/TicketNumberRequest.cs
@@ -6,7 +6,5 @@ public class TicketNumberRequest
     public int TicketGroupId { get; set; }
     public TicketType TicketType { get; set; } = TicketType.Regular;
     public int CompanyId { get; set; }
-    public bool Called { get; set; } = false;
-    public bool Serviced { get; set; } = false;
     public int SpotId { get; set; } 
 }
