6df892d22df0c807c259d488e8e7bc58d7ac6398
ivanmartinho <96298254+ivanmartinho@users.noreply.github.com>
2026-03-28 09:02:10 -0300

    Criado construtor para redução de código

--- DTOs/TicketNumberResponse.cs
+++ DTOs/TicketNumberResponse.cs
@@ -5,4 +5,12 @@ public class TicketNumberResponse
     public string TicketNumber { get; set; }
     public string SpotName { get; set; }
     public string TicketType { get; set; }
+
+    public TicketNumberResponse(int id, string ticketNumber, string spotName, string ticketType)
+    {
+        Id = id;
+        TicketNumber = ticketNumber;
+        SpotName = spotName;
+        TicketNumber = ticketNumber;
+    }
 }
