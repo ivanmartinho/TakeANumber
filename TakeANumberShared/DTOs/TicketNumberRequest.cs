ce49d4c0580645eba63c924a3544bfddc218c91e
ivanmartinho <96298254+ivanmartinho@users.noreply.github.com>
2026-03-30 08:45:34 -0300

    Restruturação do projeto para escalabidade, reuso de código

---
+++ TakeANumberShared/DTOs/TicketNumberRequest.cs
@@ -0,0 +1,10 @@
+﻿using TakeANumberApi.Enums;
+
+namespace TakeANumberApi.DTOs;
+public class TicketNumberRequest
+{
+    public int TicketGroupId { get; set; }
+    public TicketType TicketType { get; set; } = TicketType.Regular;
+    public int CompanyId { get; set; }
+    public int SpotId { get; set; } 
+}
